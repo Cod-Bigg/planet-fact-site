891bf5a04919b21df972202df91e77ffed18369b
Cody <codyabiggsweb@gmail.com>
2022-03-03 13:16:28 -0500

    made fixes to navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -9,37 +9,37 @@ class Navigation extends Component{
     render(){
         return(
             <div>
-   <nav class="navbar navbar-expand-lg navbar-dark">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon "></span>
+   <nav className="navbar navbar-expand-lg navbar-dark">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon "></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink class="navbar-brand" to="/">Planets</NavLink>
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <NavLink class="nav-link active" aria-current="page" to="/">Home</NavLink>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <NavLink className="navbar-brand" to="/">Planets</NavLink>
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/earth">Earth</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/earth">Earth</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/jupiter">Jupiter</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/jupiter">Jupiter</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/venus">Venus</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/venus">Venus</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/mars">Mars</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/mars">Mars</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/saturn">Saturn</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/saturn">Saturn</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/uranus">Uranus</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/uranus">Uranus</NavLink>
         </li>
-        <li class="nav-item">
-          <NavLink class="nav-link" to="/neptune">Neptune</NavLink>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/neptune">Neptune</NavLink>
         </li>
       </ul>
     </div>
