67bc6128118c379e5d07080f92c8bfc4be9314c3
Cody <codyabiggsweb@gmail.com>
2022-03-05 13:09:54 -0500

    re made navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,53 +1,83 @@
-import React, {Component} from "react";
-import './index.css';
-import {NavLink} from "react-router-dom"
+import { useRef } from "react";
+import { NavLink } from "react-router-dom";
 
-class Navigation extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-   <nav className="navbar navbar-expand-lg navbar-dark">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon "></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink className="navbar-brand" to="/">Planets</NavLink>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link " aria-current="page" to="/">Mercury</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/earth">Earth</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/jupiter">Jupiter</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/venus">Venus</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/mars">Mars</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/saturn">Saturn</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/uranus">Uranus</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/neptune">Neptune</NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</div>
-        )
-    }
-}
+export const Nav = () => {
+  const navButton = useRef(null);
+  const linksContainerRef = useRef(null);
 
-export default Navigation;
+  function collapseNav() {
+    navButton.current.classList.add("collapsed");
+    linksContainerRef.current.classList.remove("show");
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg sticky-top navbar-light navbar-dark">
+      <div className="container-fluid">
+        <NavLink onClick={collapseNav} className="navbar-brand" to="/">
+          Navbar
+        </NavLink>
+        <button
+          ref={navButton}
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          ref={linksContainerRef}
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                onClick={collapseNav}
+                className="nav-link active"
+                aria-current="page"
+                to="/"
+              >
+                Mercury
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink onClick={collapseNav} className="nav-link" to="/venus">
+                Venus
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink onClick={collapseNav} className="nav-link" to="/earth">
+                Earth
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink onClick={collapseNav} className="nav-link" to="/mars">
+                Mars
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink onClick={collapseNav} className="nav-link" to="/jupiter">
+                Jupiter
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink onClick={collapseNav} className="nav-link" to="/saturn">
+                Saturn
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink onClick={collapseNav} className="nav-link" to="/Neptune">
+                Neptune
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nav;
